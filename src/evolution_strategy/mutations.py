9d0fc039c7e9dfe10049f347127b0161b33d6274
lucasgomes <lgomes@post.com>
2023-09-17 20:07:52 -0300

    adding more mutation methods

--- src/evolution_strategy/mutations.py
+++ src/evolution_strategy/mutations.py
@@ -16,7 +16,7 @@ def shift_with_mutation_factor(
         individual: pd.Series, dataset: pd.DataFrame, mutation_factor: float, shift_number: int
 ) -> Series:
     for i in range(0, shift_number):
-        if round(random.uniform(0, 1), 1) <= mutation_factor:
+        if round(random.uniform(0, 1), 2) <= mutation_factor:
             shift_position = random.randint(0, 119)
             new_slope = dataset[(dataset['TAL'] == shift_position + 1)].sample(n=1)
             individual['solution'][shift_position] = new_slope['PRE'].values[0]
@@ -27,14 +27,14 @@ def shift_all(
         individual: pd.Series, dataset: pd.DataFrame, mutation_factor: float
 ) -> Series:
     for i in range(0, 120):
-        if round(random.uniform(0, 1), 1) <= mutation_factor:
+        if round(random.uniform(0, 1), 2) <= mutation_factor:
             new_slope = dataset[(dataset['TAL'] == i + 1)].sample(n=1)
             individual['solution'][i] = new_slope['PRE'].values[0]
     return individual
 
 
 def mutation(individual: pd.Series, dataset: pd.DataFrame, mutation_factor: float, shift_number: int) -> pd.Series:
-    match random.randint(1, 5):
+    match random.randint(1, 6):
         case 1:
             return shift1(individual, dataset)
         case 2:
@@ -44,6 +44,8 @@ def mutation(individual: pd.Series, dataset: pd.DataFrame, mutation_factor: floa
         case 4:
             return shift_with_mutation_factor(individual, dataset, mutation_factor, 10)
         case 5:
+            return shift_with_mutation_factor(individual, dataset, mutation_factor, 15)
+        case 6:
             return shift_all(individual, dataset, mutation_factor)
         case _:
             return individual
